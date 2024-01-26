4aed7582dac803aca8ebe1f238035f8b0f253760
AGQA2024 <codesoftware2579@gmail.com>
2024-01-26 14:49:53 +0000

    Adding all files.

---
+++ JSExercises/QL2Files/index.js
@@ -0,0 +1,35 @@
+// Aritmetic Operators
+console.log(5 + 5);
+console.log(5 * 10);
+console.log(10 % 3);
+console.log(5 + 10 / 2 * 5 - 10);
+console.log((6 + 10) / 2 * 5 - 10);
+
+// Assignment Operators
+// Assume that x is initiated as 0 and statements are processed sequentially.
+let x = 0;
+
+console.log(x = x + 1);
+// console.log(x)
+console.log(x+= 1);
+// console.log(x)
+console.log(x++);
+// console.log(x)
+console.log(++x);
+// console.log(x)
+
+// Relational Operators
+console.log(5 > 3);
+console.log(3 != 3);
+console.log(3 <= 2 && 5 >2);
+console.log(!5>3);
+
+// Mismatched Types
+// console.log(5 + "5");
+// console.log(5 + true);
+// console.log(5 * "5");
+// console.log(1 == true);
+// console.log(1 === true);
+
+ 
+
