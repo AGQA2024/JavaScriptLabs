5d4df60dc1fd06ba7ae80559bfed357737cea6a0
AGQA2024 <codesoftware2579@gmail.com>
2024-01-29 11:01:37 +0000

    Updating files at break.

---
+++ JSExercises/QL8Files/index.js
@@ -0,0 +1,15 @@
+ // Defining a new object:
+
+ let darthVader = {
+    "allegiance": "Empire",
+    "weapon": "lightsabre",
+    "sith": true
+ }
+
+console.log(darthVader.allegiance)
+console.log(darthVader.weapon)
+console.log(darthVader.sith)
+
+
+console.log(darthVader.sith == "jedi")
+console.log(darthVader.sith == "sith")
