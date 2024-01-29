9e2dedbc893d7174465f5340dcd39061252e6717
AGQA2024 <codesoftware2579@gmail.com>
2024-01-29 15:12:41 +0000

    Uploading finished exercise 9 file.

--- JSExercises/QL9Files/index.js
+++ JSExercises/QL9Files/index.js
@@ -1,6 +1,32 @@
 const colours = ['red', 'green', 'blue', 'orange', 'lime','olive', 'black', 'yellow']; 
 
-function buildP(placeholder){
-    createElement();
+function buildP(placeholder, num){
+    
+    let i = 0;
+
+    do {
+            // Order of DOM programming:
+
+            // Create the new element
+            let p = document.createElement('p');
+
+            // Create any text nodes that need to be attached to the new element
+            let text = document.createTextNode(`Have you tried turning it off and back on again?`);
+
+            // Randomising colours:
+            p.style.color = colours[parseInt(Math.random() * colours.length)]
+            
+            // Attach the text nodes to the new element
+            p.appendChild(text);
+
+            // Attach the new element to the appropriate existing DOM element
+            placeholder.appendChild(p);
+
+            // Condition to iterate through i:
+            i++;
+    
+        } while (i < num);
     
 };
+
+buildP(document.querySelector('#placeholder'), 5);
