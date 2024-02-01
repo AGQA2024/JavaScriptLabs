2b4065be3f0ca80da1087cebbaaa6da539130d9a
AGQA2024 <codesoftware2579@gmail.com>
2024-02-01 15:45:59 +0000

    Pushing tutor files.

--- JSLessonTasks/lesson/export.js
+++ JSLessonTasks/lesson/export.js
@@ -5,7 +5,17 @@ let object = {
     "stuff": [1,2,3,4,5,6,7,8],
     "action": function(){
         alert(123); 
-    }
+    },
+
+    "OtherObject": {
+        "A": "1", 
+        "B": "2",
+        "C": "3",
+        "NestedObject": {
+            "E": "4", 
+            "F": "5",
+            "G": "6"}
+        }
 }
 
 // Modify the object in some way:
